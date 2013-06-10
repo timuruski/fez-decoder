6c184ef042cafbf361758dfd18b6ed79fa5d31ae
Tim Uruski <github@login.timuruski.name>
2013-06-09 19:02:09 -0600

    Generate new Rails 4 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FezDecoder::Application.config.secret_key_base = 'f4156818fbb636cba1669c894254f7d1a99b8320fc5c379ab0c723b69f5d5fa163bca03cc2bbedd20feec27c389fd0552cf831bf43981166fd19f1961785df94'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FezDecoder::Application.config.session_store :cookie_store, key: '_fez-decoder_session'
